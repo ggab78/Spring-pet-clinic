e34c9a5fe56a73ddcf2f473ffbf6ece2c7288ff4
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-03-20 21:26:08 +0100

    create Speciality entity , associate with Vet. closes #26.

--- pet-clinic-data/src/main/java/com/gabriel/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/gabriel/springpetclinic/model/Vet.java
@@ -1,6 +1,10 @@
 package com.gabriel.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
 
     public Vet() {
     }
@@ -11,4 +15,11 @@ public class Vet extends Person {
     }
 
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
