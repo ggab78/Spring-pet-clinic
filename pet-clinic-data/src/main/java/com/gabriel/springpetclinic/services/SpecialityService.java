0b8892eee72f73ce98ce18b44e66e9c2b25657e7
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-03-21 22:20:58 +0100

    create SpecialityServiceMap. Closes #30.

---
+++ pet-clinic-data/src/main/java/com/gabriel/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.gabriel.springpetclinic.services;
+
+import com.gabriel.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/gabriel/springpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,7 @@
+package com.gabriel.springpetclinic.services.map;
+
+import com.gabriel.springpetclinic.model.Speciality;
+import com.gabriel.springpetclinic.services.SpecialityService;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality,Long> implements SpecialityService {
+}
