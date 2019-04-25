bcaa77d4f933d6accef6f1a94fe2265c463695a4
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-04-25 17:29:43 +0200

    set web data binder to disallow id modification

--- pet-clinic-web/src/main/java/com/gabriel/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/gabriel/springpetclinic/controllers/OwnerController.java
@@ -3,6 +3,8 @@ package com.gabriel.springpetclinic.controllers;
 import com.gabriel.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +17,11 @@ private final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"","/","/index","/index.html"})
