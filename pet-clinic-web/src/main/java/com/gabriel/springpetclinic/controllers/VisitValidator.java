559a5f1df563f5e9f488579410fec1a8a85cade3
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-12-14 09:44:32 +0100

    visit validator added Closes #64

---
+++ pet-clinic-web/src/main/java/com/gabriel/springpetclinic/controllers/VisitValidator.java
@@ -0,0 +1,39 @@
+package com.gabriel.springpetclinic.controllers;
+
+import com.gabriel.springpetclinic.model.Visit;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.time.LocalDate;
+
+public class VisitValidator implements Validator {
+
+    private static final String REQUIRED = "required";
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Visit.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+        Visit visit = (Visit) obj;
+        String description = visit.getDescription();
+        // description validation
+        if (!StringUtils.hasLength(description)) {
+            errors.rejectValue("description", REQUIRED, REQUIRED);
+        }
+
+        // birth date validation
+        if (visit.getDate() == null) {
+            errors.rejectValue("date", REQUIRED, REQUIRED);
+        }
+
+        // birth date not later then
+        LocalDate now = LocalDate.now();
+        if(visit.getDate().compareTo(now)<0){
+            errors.rejectValue("date",REQUIRED, REQUIRED);
+        }
+    }
+}
